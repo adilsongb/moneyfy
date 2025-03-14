9314c3690f63aed7918e26d861c102c8729d9d07
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2025-03-14 14:32:12 -0300

    fix: import resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
   rules: {
     "prettier/prettier": "error",
+    "react-hooks/exhaustive-deps": "off",
   },
   ignorePatterns: ["/dist/*"],
 };
